197d617ef3e32bc85142a7e497ca3f31cda350e7
AndelsonOliveira1995 <JOliveiraC1995@gmail.com>
2024-09-10 16:26:13 -0300

    Update admin.blade.php

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+{{-- AUTHOR: Andelson Bráulio Jaime de Oliveira --}}
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
